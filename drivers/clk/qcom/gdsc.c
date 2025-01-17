405d48e97af60b7e6f3d6d0cf527be7210deb10a
Bryan O'Donoghue <bryan.odonoghue@linaro.org>
2025-01-17 13:54:08 +0000

    clk: qcom: gdsc: Capture pm_genpd_add_subdomain result code
    commit 65a733464553ea192797b889d1533a1a37216f32 upstream.
    
    Adding a new clause to this if/else I noticed the existing usage of
    pm_genpd_add_subdomain() wasn't capturing and returning the result code.
    
    pm_genpd_add_subdomain() returns an int and can fail. Capture that result
    code and throw it up the call stack if something goes wrong.
    
    Fixes: 1b771839de05 ("clk: qcom: gdsc: enable optional power domain support")
    Cc: stable@vger.kernel.org
    Signed-off-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Link: https://lore.kernel.org/r/20250117-b4-linux-next-24-11-18-clock-multiple-power-domains-v10-2-13f2bb656dad@linaro.org
    Signed-off-by: Bjorn Andersson <andersson@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/clk/qcom/gdsc.c
+++ drivers/clk/qcom/gdsc.c
@@ -548,6 +548,23 @@ err_disable_supply:
 	return ret;
 }
 
+static void gdsc_pm_subdomain_remove(struct gdsc_desc *desc, size_t num)
+{
+	struct device *dev = desc->dev;
+	struct gdsc **scs = desc->scs;
+	int i;
+
+	/* Remove subdomains */
+	for (i = num - 1; i >= 0; i--) {
+		if (!scs[i])
+			continue;
+		if (scs[i]->parent)
+			pm_genpd_remove_subdomain(scs[i]->parent, &scs[i]->pd);
+		else if (!IS_ERR_OR_NULL(dev->pm_domain))
+			pm_genpd_remove_subdomain(pd_to_genpd(dev->pm_domain), &scs[i]->pd);
+	}
+}
+
 int gdsc_register(struct gdsc_desc *desc,
 		  struct reset_controller_dev *rcdev, struct regmap *regmap)
 {
@@ -606,26 +623,36 @@ int gdsc_register(struct gdsc_desc *desc,
 		if (!scs[i])
 			continue;
 		if (scs[i]->parent)
-			pm_genpd_add_subdomain(scs[i]->parent, &scs[i]->pd);
+			ret = pm_genpd_add_subdomain(scs[i]->parent, &scs[i]->pd);
 		else if (!IS_ERR_OR_NULL(dev->pm_domain))
+<<<<<<< HEAD
 			pm_genpd_add_subdomain(pd_to_genpd(dev->pm_domain), &scs[i]->pd);
 
 		ret = gdsc_genpd_debug_register(scs[i]);
 		if (ret)
 			dev_warn(dev, "Failed to register debugfs for %s ret=%d\n",
 							scs[i]->pd.name, ret);
+=======
+			ret = pm_genpd_add_subdomain(pd_to_genpd(dev->pm_domain), &scs[i]->pd);
+		if (ret)
+			goto err_pm_subdomain_remove;
+>>>>>>> 4d6fb2a43f0d (clk: qcom: gdsc: Capture pm_genpd_add_subdomain result code)
 	}
 
 	return of_genpd_add_provider_onecell(dev->of_node, data);
+
+err_pm_subdomain_remove:
+	gdsc_pm_subdomain_remove(desc, i);
+
+	return ret;
 }
 
 void gdsc_unregister(struct gdsc_desc *desc)
 {
-	int i;
 	struct device *dev = desc->dev;
-	struct gdsc **scs = desc->scs;
 	size_t num = desc->num;
 
+<<<<<<< HEAD
 	/* Remove subdomains */
 	for (i = num - 1; i >= 0; i--) {
 		if (!scs[i])
@@ -638,6 +665,9 @@ void gdsc_unregister(struct gdsc_desc *desc)
 		else if (!IS_ERR_OR_NULL(dev->pm_domain))
 			pm_genpd_remove_subdomain(pd_to_genpd(dev->pm_domain), &scs[i]->pd);
 	}
+=======
+	gdsc_pm_subdomain_remove(desc, num);
+>>>>>>> 4d6fb2a43f0d (clk: qcom: gdsc: Capture pm_genpd_add_subdomain result code)
 	of_genpd_del_provider(dev->of_node);
 }
 
