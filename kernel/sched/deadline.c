47b14902f3f10fb8181054634852230487f72f9d
Harshit Agarwal <harshit@nutanix.com>
2025-04-08 04:50:21 +0000

    sched/deadline: Fix race in push_dl_task()
    commit 8fd5485fb4f3d9da3977fd783fcb8e5452463420 upstream.
    
    When a CPU chooses to call push_dl_task and picks a task to push to
    another CPU's runqueue then it will call find_lock_later_rq method
    which would take a double lock on both CPUs' runqueues. If one of the
    locks aren't readily available, it may lead to dropping the current
    runqueue lock and reacquiring both the locks at once. During this window
    it is possible that the task is already migrated and is running on some
    other CPU. These cases are already handled. However, if the task is
    migrated and has already been executed and another CPU is now trying to
    wake it up (ttwu) such that it is queued again on the runqeue
    (on_rq is 1) and also if the task was run by the same CPU, then the
    current checks will pass even though the task was migrated out and is no
    longer in the pushable tasks list.
    Please go through the original rt change for more details on the issue.
    
    To fix this, after the lock is obtained inside the find_lock_later_rq,
    it ensures that the task is still at the head of pushable tasks list.
    Also removed some checks that are no longer needed with the addition of
    this new check.
    However, the new check of pushable tasks list only applies when
    find_lock_later_rq is called by push_dl_task. For the other caller i.e.
    dl_task_offline_migration, existing checks are used.
    
    Signed-off-by: Harshit Agarwal <harshit@nutanix.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Acked-by: Juri Lelli <juri.lelli@redhat.com>
    Cc: stable@vger.kernel.org
    Link: https://lore.kernel.org/r/20250408045021.3283624-1-harshit@nutanix.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2644,12 +2644,17 @@ static int find_later_rq(struct task_struct *sched_ctx, struct task_struct *exec
 
 static struct task_struct *pick_next_pushable_dl_task(struct rq *rq)
 {
+<<<<<<< HEAD
 	struct task_struct *p = NULL;
 	struct rb_node *next_node;
+=======
+	struct task_struct *p;
+>>>>>>> 305b1a39f3bb (sched/deadline: Fix race in push_dl_task())
 
 	if (!has_pushable_dl_tasks(rq))
 		return NULL;
 
+<<<<<<< HEAD
 	next_node = rb_first_cached(&rq->dl.pushable_dl_tasks_root);
 	while (next_node) {
 		p = __node_2_pdl(next_node);
@@ -2667,6 +2672,9 @@ static struct task_struct *pick_next_pushable_dl_task(struct rq *rq)
 
 	if (!p)
 		return NULL;
+=======
+	p = __node_2_pdl(rb_first_cached(&rq->dl.pushable_dl_tasks_root));
+>>>>>>> 305b1a39f3bb (sched/deadline: Fix race in push_dl_task())
 
 	WARN_ON_ONCE(rq->cpu != task_cpu(p));
 	WARN_ON_ONCE(task_current(rq, p));
@@ -2678,6 +2686,7 @@ static struct task_struct *pick_next_pushable_dl_task(struct rq *rq)
 	return p;
 }
 
+<<<<<<< HEAD
 static inline bool __dl_revalidate_rq_state(struct task_struct *task, struct rq *rq,
 					    struct rq *later, bool *retry)
 {
@@ -2731,6 +2740,8 @@ static inline bool dl_revalidate_rq_state(struct task_struct *task, struct rq *r
 	return true;
 }
 
+=======
+>>>>>>> 305b1a39f3bb (sched/deadline: Fix race in push_dl_task())
 /* Locks the rq it finds */
 static struct rq *find_lock_later_rq(struct task_struct *task, struct rq *rq)
 {
@@ -2764,7 +2775,41 @@ static struct rq *find_lock_later_rq(struct task_struct *task, struct rq *rq)
 
 		/* Retry if something changed. */
 		if (double_lock_balance(rq, later_rq)) {
+<<<<<<< HEAD
 			if (unlikely(!dl_revalidate_rq_state(task, rq, later_rq, &retry))) {
+=======
+			/*
+			 * double_lock_balance had to release rq->lock, in the
+			 * meantime, task may no longer be fit to be migrated.
+			 * Check the following to ensure that the task is
+			 * still suitable for migration:
+			 * 1. It is possible the task was scheduled,
+			 *    migrate_disabled was set and then got preempted,
+			 *    so we must check the task migration disable
+			 *    flag.
+			 * 2. The CPU picked is in the task's affinity.
+			 * 3. For throttled task (dl_task_offline_migration),
+			 *    check the following:
+			 *    - the task is not on the rq anymore (it was
+			 *      migrated)
+			 *    - the task is not on CPU anymore
+			 *    - the task is still a dl task
+			 *    - the task is not queued on the rq anymore
+			 * 4. For the non-throttled task (push_dl_task), the
+			 *    check to ensure that this task is still at the
+			 *    head of the pushable tasks list is enough.
+			 */
+			if (unlikely(is_migration_disabled(task) ||
+				     !cpumask_test_cpu(later_rq->cpu, &task->cpus_mask) ||
+				     (task->dl.dl_throttled &&
+				      (task_rq(task) != rq ||
+				       task_on_cpu(rq, task) ||
+				       !dl_task(task) ||
+				       !task_on_rq_queued(task))) ||
+				     (!task->dl.dl_throttled &&
+				      task != pick_next_pushable_dl_task(rq)))) {
+
+>>>>>>> 305b1a39f3bb (sched/deadline: Fix race in push_dl_task())
 				double_unlock_balance(rq, later_rq);
 				later_rq = NULL;
 				break;
