46bb7de36dfea706b55dc1ec5aff55958f727a53
Prashanth K <prashanth.k@oss.qualcomm.com>
2025-04-22 16:02:31 +0530

    usb: dwc3: gadget: Make gadget_wakeup asynchronous
    commit 2372f1caeca433c4c01c2482f73fbe057f5168ce upstream.
    
    Currently gadget_wakeup() waits for U0 synchronously if it was
    called from func_wakeup(), this is because we need to send the
    function wakeup command soon after the link is active. And the
    call is made synchronous by polling DSTS continuosly for 20000
    times in __dwc3_gadget_wakeup(). But it observed that sometimes
    the link is not active even after polling 20K times, leading to
    remote wakeup failures. Adding a small delay between each poll
    helps, but that won't guarantee resolution in future. Hence make
    the gadget_wakeup completely asynchronous.
    
    Since multiple interfaces can issue a function wakeup at once,
    add a new variable wakeup_pending_funcs which will indicate the
    functions that has issued func_wakup, this is represented in a
    bitmap format. If the link is in U3, dwc3_gadget_func_wakeup()
    will set the bit corresponding to interface_id and bail out.
    Once link comes back to U0, linksts_change irq is triggered,
    where the function wakeup command is sent based on bitmap.
    
    Cc: stable <stable@kernel.org>
    Fixes: 92c08a84b53e ("usb: dwc3: Add function suspend and function wakeup support")
    Signed-off-by: Prashanth K <prashanth.k@oss.qualcomm.com>
    Acked-by: Thinh Nguyen <Thinh.Nguyen@synopsys.com>
    Link: https://lore.kernel.org/r/20250422103231.1954387-4-prashanth.k@oss.qualcomm.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/dwc3/core.h
+++ drivers/usb/dwc3/core.h
@@ -1181,6 +1181,9 @@ struct dwc3_scratchpad_array {
  * @gsbuscfg0_reqinfo: store GSBUSCFG0.DATRDREQINFO, DESRDREQINFO,
  *		       DATWRREQINFO, and DESWRREQINFO value passed from
  *		       glue driver.
+ * @wakeup_pending_funcs: Indicates whether any interface has requested for
+ *			 function wakeup in bitmap format where bit position
+ *			 represents interface_id.
  */
 struct dwc3 {
 	struct work_struct	drd_work;
@@ -1412,11 +1415,15 @@ struct dwc3 {
 	int			num_ep_resized;
 	struct dentry		*debug_root;
 	u32			gsbuscfg0_reqinfo;
+<<<<<<< HEAD
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
 	ANDROID_KABI_RESERVE(3);
 	ANDROID_KABI_RESERVE(4);
+=======
+	u32			wakeup_pending_funcs;
+>>>>>>> f6fdbe4d543b (usb: dwc3: gadget: Make gadget_wakeup asynchronous)
 };
 
 #define INCRX_BURST_MODE 0
