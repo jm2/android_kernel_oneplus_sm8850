1193544c7732bb109346b0decd0b13de6c9f7625
Seunghui Lee <sh043.lee@samsung.com>
2025-07-17 17:12:13 +0900

    scsi: ufs: core: Use link recovery when h8 exit fails during runtime resume
    [ Upstream commit 35dabf4503b94a697bababe94678a8bc989c3223 ]
    
    If the h8 exit fails during runtime resume process, the runtime thread
    enters runtime suspend immediately and the error handler operates at the
    same time.  It becomes stuck and cannot be recovered through the error
    handler.  To fix this, use link recovery instead of the error handler.
    
    Fixes: 4db7a2360597 ("scsi: ufs: Fix concurrency of error handler and other error recovery paths")
    Signed-off-by: Seunghui Lee <sh043.lee@samsung.com>
    Link: https://lore.kernel.org/r/20250717081213.6811-1-sh043.lee@samsung.com
    Reviewed-by: Bean Huo <beanhuo@micron.com>
    Acked-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: Martin K. Petersen <martin.petersen@oracle.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -4448,9 +4448,15 @@ out_unlock:
 	mutex_unlock(&hba->uic_cmd_mutex);
 
 	/*
+<<<<<<< HEAD
 	 * If the h8 exit fails during the runtime resume process,
 	 * it becomes stuck and cannot be recovered through the error handler.
 	 * To fix this, use link recovery instead of the error handler.
+=======
+	 * If the h8 exit fails during the runtime resume process, it becomes
+	 * stuck and cannot be recovered through the error handler.  To fix
+	 * this, use link recovery instead of the error handler.
+>>>>>>> d710ed68c54a (scsi: ufs: core: Use link recovery when h8 exit fails during runtime resume)
 	 */
 	if (ret && hba->pm_op_in_progress)
 		ret = ufshcd_link_recovery(hba);
