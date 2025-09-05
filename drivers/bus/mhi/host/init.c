d382ca87ba5f6873ac59fe3e54850530a4562f1d
Adam Xue <zxue@semtech.com>
2025-09-05 10:41:18 -0700

    bus: mhi: host: Do not use uninitialized 'dev' pointer in mhi_init_irq_setup()
    commit d0856a6dff57f95cc5d2d74e50880f01697d0cc4 upstream.
    
    In mhi_init_irq_setup, the device pointer used for dev_err() was not
    initialized. Use the pointer from mhi_cntrl instead.
    
    Fixes: b0fc0167f254 ("bus: mhi: core: Allow shared IRQ for event rings")
    Fixes: 3000f85b8f47 ("bus: mhi: core: Add support for basic PM operations")
    Signed-off-by: Adam Xue <zxue@semtech.com>
    [mani: reworded subject/description and CCed stable]
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@oss.qualcomm.com>
    Reviewed-by: Krishna Chaitanya Chundru <krishna.chundru@oss.qualcomm.com>
    Cc: stable@vger.kernel.org
    Link: https://patch.msgid.link/20250905174118.38512-1-zxue@semtech.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/bus/mhi/host/init.c
+++ drivers/bus/mhi/host/init.c
@@ -195,7 +195,6 @@ void mhi_deinit_free_irq(struct mhi_controller *mhi_cntrl)
 int mhi_init_irq_setup(struct mhi_controller *mhi_cntrl)
 {
 	struct mhi_event *mhi_event = mhi_cntrl->mhi_event;
-	struct device *dev = &mhi_cntrl->mhi_dev->dev;
 	unsigned long irq_flags = IRQF_SHARED | IRQF_NO_SUSPEND;
 	int i, ret;
 
@@ -222,7 +221,11 @@ int mhi_init_irq_setup(struct mhi_controller *mhi_cntrl)
 			continue;
 
 		if (mhi_event->irq >= mhi_cntrl->nr_irqs) {
+<<<<<<< HEAD
 			MHI_ERR(dev, "irq %d not available for event ring\n",
+=======
+			dev_err(mhi_cntrl->cntrl_dev, "irq %d not available for event ring\n",
+>>>>>>> 61a60c45ebd6 (bus: mhi: host: Do not use uninitialized 'dev' pointer in mhi_init_irq_setup())
 				mhi_event->irq);
 			ret = -EINVAL;
 			goto error_request;
@@ -233,7 +236,11 @@ int mhi_init_irq_setup(struct mhi_controller *mhi_cntrl)
 				  irq_flags,
 				  "mhi", mhi_event);
 		if (ret) {
+<<<<<<< HEAD
 			MHI_ERR(dev, "Error requesting irq:%d for ev:%d\n",
+=======
+			dev_err(mhi_cntrl->cntrl_dev, "Error requesting irq:%d for ev:%d\n",
+>>>>>>> 61a60c45ebd6 (bus: mhi: host: Do not use uninitialized 'dev' pointer in mhi_init_irq_setup())
 				mhi_cntrl->irq[mhi_event->irq], i);
 			goto error_request;
 		}
