742074cdd4c121d2b48a159e2a02f1c76359d9f9
Hyesoo Yu <hyesoo.yu@samsung.com>
2025-09-06 17:25:28 -0400

    mm: slub: call WARN() when detecting a slab corruption
    [ Upstream commit 3f6f32b14ab35452d2ed52f7821cf2829923c98d ]
    
    If a slab object is corrupted or an error occurs in its internal
    validation, continuing after restoration may cause other side effects.
    At this point, it is difficult to debug because the problem occurred in
    the past. It is useful to use WARN() to catch errors at the point of
    issue because WARN() could trigger panic for system debugging when
    panic_on_warn is enabled. WARN() is added where to detect the error on
    slab_err and object_err.
    
    It makes sense to only do the WARN() after printing the logs. slab_err
    is splited to __slab_err that calls the WARN() and it is called after
    printing logs.
    
    Signed-off-by: Hyesoo Yu <hyesoo.yu@samsung.com>
    Reviewed-by: Harry Yoo <harry.yoo@oracle.com>
    Signed-off-by: Vlastimil Babka <vbabka@suse.cz>
    Stable-dep-of: b4efccec8d06 ("mm/slub: avoid accessing metadata when pointer is invalid in object_err()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/slub.c
+++ mm/slub.c
@@ -1186,7 +1186,11 @@ static __printf(3, 4) void slab_err(struct kmem_cache *s, struct slab *slab,
 	va_start(args, fmt);
 	__slab_bug(s, fmt, args);
 	va_end(args);
+<<<<<<< HEAD
 
+=======
+	slab_bug(s, "%s", buf);
+>>>>>>> d06b739f41dc (mm: slub: call WARN() when detecting a slab corruption)
 	__slab_err(slab);
 }
 
