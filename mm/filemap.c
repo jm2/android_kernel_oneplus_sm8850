68c97f0d8fadacf9df40785c21181aafa3400daf
Kiryl Shutsemau <kas@kernel.org>
2025-10-27 11:56:35 +0000

    mm/memory: do not populate page table entries beyond i_size
    commit 74207de2ba10c2973334906822dc94d2e859ffc5 upstream.
    
    Patch series "Fix SIGBUS semantics with large folios", v3.
    
    Accessing memory within a VMA, but beyond i_size rounded up to the next
    page size, is supposed to generate SIGBUS.
    
    Darrick reported[1] an xfstests regression in v6.18-rc1.  generic/749
    failed due to missing SIGBUS.  This was caused by my recent changes that
    try to fault in the whole folio where possible:
    
            19773df031bc ("mm/fault: try to map the entire file folio in finish_fault()")
            357b92761d94 ("mm/filemap: map entire large folio faultaround")
    
    These changes did not consider i_size when setting up PTEs, leading to
    xfstest breakage.
    
    However, the problem has been present in the kernel for a long time -
    since huge tmpfs was introduced in 2016.  The kernel happily maps
    PMD-sized folios as PMD without checking i_size.  And huge=always tmpfs
    allocates PMD-size folios on any writes.
    
    I considered this corner case when I implemented a large tmpfs, and my
    conclusion was that no one in their right mind should rely on receiving a
    SIGBUS signal when accessing beyond i_size.  I cannot imagine how it could
    be useful for the workload.
    
    But apparently filesystem folks care a lot about preserving strict SIGBUS
    semantics.
    
    Generic/749 was introduced last year with reference to POSIX, but no real
    workloads were mentioned.  It also acknowledged the tmpfs deviation from
    the test case.
    
    POSIX indeed says[3]:
    
            References within the address range starting at pa and
            continuing for len bytes to whole pages following the end of an
            object shall result in delivery of a SIGBUS signal.
    
    The patchset fixes the regression introduced by recent changes as well as
    more subtle SIGBUS breakage due to split failure on truncation.
    
    This patch (of 2):
    
    Accesses within VMA, but beyond i_size rounded up to PAGE_SIZE are
    supposed to generate SIGBUS.
    
    Recent changes attempted to fault in full folio where possible.  They did
    not respect i_size, which led to populating PTEs beyond i_size and
    breaking SIGBUS semantics.
    
    Darrick reported generic/749 breakage because of this.
    
    However, the problem existed before the recent changes.  With huge=always
    tmpfs, any write to a file leads to PMD-size allocation.  Following the
    fault-in of the folio will install PMD mapping regardless of i_size.
    
    Fix filemap_map_pages() and finish_fault() to not install:
      - PTEs beyond i_size;
      - PMD mappings across i_size;
    
    Make an exception for shmem/tmpfs that for long time intentionally
    mapped with PMDs across i_size.
    
    Link: https://lkml.kernel.org/r/20251027115636.82382-1-kirill@shutemov.name
    Link: https://lkml.kernel.org/r/20251027115636.82382-2-kirill@shutemov.name
    Signed-off-by: Kiryl Shutsemau <kas@kernel.org>
    Fixes: 6795801366da ("xfs: Support large folios")
    Reported-by: "Darrick J. Wong" <djwong@kernel.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Christian Brauner <brauner@kernel.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: David Hildenbrand <david@redhat.com>
    Cc: Hugh Dickins <hughd@google.com>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Rik van Riel <riel@surriel.com>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Kiryl Shutsemau <kas@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -3708,8 +3708,12 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 	vm_fault_t ret = 0;
 	unsigned long rss = 0;
 	unsigned int nr_pages = 0, mmap_miss = 0, mmap_miss_saved, folio_type;
+<<<<<<< HEAD
 	pgoff_t first_pgoff = 0;
 	pgoff_t orig_start_pgoff = start_pgoff;
+=======
+	bool can_map_large;
+>>>>>>> c4476fac0c6c (mm/memory: do not populate page table entries beyond i_size)
 
 	rcu_read_lock();
 	folio = next_uptodate_folio(&xas, mapping, end_pgoff);
@@ -3718,7 +3722,20 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 	first_pgoff = xas.xa_index;
 	orig_start_pgoff = xas.xa_index;
 
-	if (filemap_map_pmd(vmf, folio, start_pgoff)) {
+	file_end = DIV_ROUND_UP(i_size_read(mapping->host), PAGE_SIZE) - 1;
+	end_pgoff = min(end_pgoff, file_end);
+
+	/*
+	 * Do not allow to map with PTEs beyond i_size and with PMD
+	 * across i_size to preserve SIGBUS semantics.
+	 *
+	 * Make an exception for shmem/tmpfs that for long time
+	 * intentionally mapped with PMDs across i_size.
+	 */
+	can_map_large = shmem_mapping(mapping) ||
+		file_end >= folio_next_index(folio);
+
+	if (can_map_large && filemap_map_pmd(vmf, folio, start_pgoff)) {
 		ret = VM_FAULT_NOPAGE;
 		goto out;
 	}
@@ -3730,10 +3747,6 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 		folio_put(folio);
 		goto out;
 	}
-
-	file_end = DIV_ROUND_UP(i_size_read(mapping->host), PAGE_SIZE) - 1;
-	if (end_pgoff > file_end)
-		end_pgoff = file_end;
 
 	folio_type = mm_counter_file(folio);
 	do {
