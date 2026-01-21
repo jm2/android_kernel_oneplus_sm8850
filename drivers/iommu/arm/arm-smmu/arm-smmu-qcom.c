e8cf14c4931ce3f13ea88904e3e89d6f72a2d5e3
Danilo Krummrich <dakr@kernel.org>
2026-01-21 15:12:01 +0100

    iommu/arm-smmu-qcom: do not register driver in probe()
    commit ed1ac3c977dd6b119405fa36dd41f7151bd5b4de upstream.
    
    Commit 0b4eeee2876f ("iommu/arm-smmu-qcom: Register the TBU driver in
    qcom_smmu_impl_init") intended to also probe the TBU driver when
    CONFIG_ARM_SMMU_QCOM_DEBUG is disabled, but also moved the corresponding
    platform_driver_register() call into qcom_smmu_impl_init() which is
    called from arm_smmu_device_probe().
    
    However, it neither makes sense to register drivers from probe()
    callbacks of other drivers, nor does the driver core allow registering
    drivers with a device lock already being held.
    
    The latter was revealed by commit dc23806a7c47 ("driver core: enforce
    device_lock for driver_match_device()") leading to a deadlock condition
    described in [1].
    
    Additionally, it was noted by Robin that the current approach is
    potentially racy with async probe [2].
    
    Hence, fix this by registering the qcom_smmu_tbu_driver from
    module_init(). Unfortunately, due to the vendoring of the driver, this
    requires an indirection through arm-smmu-impl.c.
    
    Reported-by: Mark Brown <broonie@kernel.org>
    Closes: https://lore.kernel.org/lkml/7ae38e31-ef31-43ad-9106-7c76ea0e8596@sirena.org.uk/
    Link: https://lore.kernel.org/lkml/DFU7CEPUSG9A.1KKGVW4HIPMSH@kernel.org/ [1]
    Link: https://lore.kernel.org/lkml/0c0d3707-9ea5-44f9-88a1-a65c62e3df8d@arm.com/ [2]
    Fixes: dc23806a7c47 ("driver core: enforce device_lock for driver_match_device()")
    Fixes: 0b4eeee2876f ("iommu/arm-smmu-qcom: Register the TBU driver in qcom_smmu_impl_init")
    Acked-by: Robin Murphy <robin.murphy@arm.com>
    Tested-by: Bjorn Andersson <andersson@kernel.org>
    Reviewed-by: Bjorn Andersson <andersson@kernel.org>
    Acked-by: Konrad Dybcio <konradybcio@kernel.org>
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Tested-by: Ioana Ciornei <ioana.ciornei@nxp.com> #LX2160ARDB
    Tested-by: Wang Jiayue <akaieurus@gmail.com>
    Reviewed-by: Wang Jiayue <akaieurus@gmail.com>
    Tested-by: Mark Brown <broonie@kernel.org>
    Acked-by: Joerg Roedel <joerg.roedel@amd.com>
    Link: https://patch.msgid.link/20260121141215.29658-1-dakr@kernel.org
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-qcom.c
@@ -2287,10 +2287,6 @@ struct arm_smmu_device *qcom_smmu_impl_init(struct arm_smmu_device *smmu)
 {
 	const struct device_node *np = smmu->dev->of_node;
 	const struct of_device_id *match;
-	static u8 tbu_registered;
-
-	if (!tbu_registered++)
-		platform_driver_register(&qcom_smmu_tbu_driver);
 
 #ifdef CONFIG_ACPI
 	if (np == NULL) {
@@ -2316,6 +2312,7 @@ struct arm_smmu_device *qcom_smmu_impl_init(struct arm_smmu_device *smmu)
 	return smmu;
 }
 
+<<<<<<< HEAD
 struct arm_smmu_device *qsmmuv2_impl_init(struct arm_smmu_device *smmu)
 {
 	struct device *dev = smmu->dev;
@@ -2341,4 +2338,14 @@ struct arm_smmu_device *qsmmuv2_impl_init(struct arm_smmu_device *smmu)
 		return ERR_PTR(ret);
 
 	return &data->smmu;
+=======
+int __init qcom_smmu_module_init(void)
+{
+	return platform_driver_register(&qcom_smmu_tbu_driver);
+}
+
+void __exit qcom_smmu_module_exit(void)
+{
+	platform_driver_unregister(&qcom_smmu_tbu_driver);
+>>>>>>> 121f787e740d (iommu/arm-smmu-qcom: do not register driver in probe())
 }
