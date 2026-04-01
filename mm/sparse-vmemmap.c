1e1c84e14db1f3a6678aa4698f04347b2223dcfb
Anshuman Khandual <anshuman.khandual@arm.com>
2026-04-01 13:06:42 -0400

    mm: replace READ_ONCE() with standard page table accessors
    [ Upstream commit c0efdb373c3aaacb32db59cadb0710cac13e44ae ]
    
    Replace all READ_ONCE() with a standard page table accessors i.e
    pxdp_get() that defaults into READ_ONCE() in cases where platform does not
    override.
    
    Link: https://lkml.kernel.org/r/20251007063100.2396936-1-anshuman.khandual@arm.com
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Reviewed-by: Lance Yang <lance.yang@linux.dev>
    Reviewed-by: Wei Yang <richard.weiyang@gmail.com>
    Reviewed-by: Dev Jain <dev.jain@arm.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Stable-dep-of: ffef67b93aa3 ("mm/memory: fix PMD/PUD checks in follow_pfnmap_start()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- mm/sparse-vmemmap.c
+++ mm/sparse-vmemmap.c
@@ -337,6 +337,7 @@ int __meminit vmemmap_populate_hugepages(unsigned long start, unsigned long end,
 			return -ENOMEM;
 
 		pmd = pmd_offset(pud, addr);
+<<<<<<< HEAD
 
 		/* avoid pmd size alloc if no altmap */
 #ifdef CONFIG_QCOM_VM_MEMORY_FEATURES_NONGKI
@@ -344,6 +345,9 @@ int __meminit vmemmap_populate_hugepages(unsigned long start, unsigned long end,
 #else
 		if (pmd_none(READ_ONCE(*pmd))) {
 #endif
+=======
+		if (pmd_none(pmdp_get(pmd))) {
+>>>>>>> 9bcbf9ae7db8 (mm: replace READ_ONCE() with standard page table accessors)
 			void *p;
 
 			p = vmemmap_alloc_block_buf(PMD_SIZE, node, altmap);
